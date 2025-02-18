a6eff39795b517bc8f73cbd5083177159ed128c4
Lorenzo Gava <llorenzzzo@users.noreply.github.com>
2025-02-18 02:22:38 -0300

    add project to repo

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1 className="text-4xl font-semibold font-heading">Hello World</h1>
+      </div>
+    </main>
+  );
+}
